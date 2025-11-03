9d817f1482f959f9d0b451acf65fc37d180985a3
Dylan Pee <peedylan94@gmail.com>
2025-11-04 10:18:32 +1030

    🔨: Load Mongo_URI .env variable with fixed path

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
-import "dotenv/config";
+import path from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 mongoose.connection.on('connected', () => console.log('Connected to Mongo'));
 
