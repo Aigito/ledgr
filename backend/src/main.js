da80fbc1b517da92b7cc8a2331926f82028aac47
Dylan Pee <peedylan94@gmail.com>
2025-11-04 08:46:47 +1030

    🚧: Add test routes for /account endpoint

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,10 +1,9 @@
 import express from "express";
+import accountRouter from "./routes/account.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hiii");
-});
+app.use("/account", accountRouter);
 
 app.listen(3000, (req, res) => {
   console.log("Listening on port 3000");
---
+++ backend/src/routes/account.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const accountRouter = express.Router();
+
+accountRouter.get("/", (req, res) => {
+  res.send("TO DO: Retrieve all accounts");
+});
+
+accountRouter.post("/", (req, res) => {
+  res.send("TO DO: Create a new account");
+});
+
+accountRouter.patch("/", (req, res) => {
+  res.send("TO DO: Update an existing account");
+});
+
+accountRouter.delete("/", (req, res) => {
+  res.send("TO DO: Delete an existing account");
+});
+
+export default accountRouter;
