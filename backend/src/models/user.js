23eee49d1ae17e8c68c9c24d79c35d226db7684d
Dylan Pee <peedylan94@gmail.com>
2025-11-04 11:38:14 +1030

    ✨: Add validations to User model

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,18 +1,41 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: true
+      required: [true, "User name is required"],
+      minLength: [3, "User name is too short, needs to be more than 3 characters"],
+      validate: {
+        validator: function (value) {
+          return /^[a-zA-Z]+$/.test(value);
+        },
+        message: "Please only use alphabetical characters in user name"
+      }
     },
     email: {
       type: String,
-      required: true
+      required: [true, "User email is required"],
+      minlength: [3, "User email is too short"],
+      lowercase: true,
+      trim: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("User email is invalid");
+        }
+      }
     },
     password: {
       type: String,
-      required: true
+      required: true,
+      validate: {
+        validator: function (value) {
+          return validator.isStrongPassword(value);
+        },
+        message: "Please ensure that password contains 1 lowercase, 1 uppercase, 1 number, 1 symbol and at least 8 characters"
+      }
     },
   },
   {
