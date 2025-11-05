11ed527dec283c994111892ab0695f00684a75ba
Dylan Pee <peedylan94@gmail.com>
2025-11-06 07:46:42 +0800

    ✨: Add account model and schema

---
+++ backend/src/models/account.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const accountSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Account must be linked to a user"]
+    },
+    accountName: {
+      type: String,
+      required: [true, "Please provide an account name"],
+      validate: {
+        validator: function (value) {
+          return /^[\w ]+$/.test(value);
+        },
+        message: "Please only use alphanumeric characters"
+      }
+    },
+    defaultType: {
+      type: String,
+      enum: {
+        values: ["debit", "credit"],
+        message: "`{VALUE}` is not a valid type, please choose debit or credit"
+      },
+      required: [true, "Please provide a default type: debit or credit"]
+    },
+    category: {
+      type: String,
+      enum: {
+        values: ["income", "expense"],
+        message: "`{VALUE} is not a valid category"
+      },
+      required: [true, "Please select a valid category"]
+    }
+  }
+);
+
+const Account = mongoose.model("Account", accountSchema);
+
+export default Account;
