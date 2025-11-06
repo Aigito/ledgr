6985a3841ab7e08d267c9ba8699a18be318a2954
Dylan Pee <peedylan94@gmail.com>
2025-11-06 08:34:03 +0800

    ✨: Implement POST route for account

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -1,14 +1,35 @@
 import express from "express";
 import userAuth from "../middlewares/userAuth.js";
+import Account from "../models/account.js"
 
 const accountRouter = express.Router();
 
 accountRouter.get("/", userAuth, (req, res) => {
-  res.send("TO DO: Retrieve all accounts");
+
 });
 
-accountRouter.post("/", (req, res) => {
-  res.send("TO DO: Create a new account");
+accountRouter.post("/", userAuth, async (req, res) => {
+  const { accountName, defaultType, category } = req.body;
+  const user = req.user;
+
+  try {
+    const account = new Account({
+      userId: user.id,
+      accountName,
+      defaultType,
+      category
+    });
+
+    const savedAccount = await account.save();
+
+    res.json({
+      message: "Account created successfully",
+      data: savedAccount
+    });
+  } catch (err) {
+    console.error(err.message);
+    console.log(`Unable to create account ${accountName}`);
+  };
 });
 
 accountRouter.patch("/", (req, res) => {
