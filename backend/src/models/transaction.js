86161c2c142913166b9b16ffb7884d4eb88794b2
Dylan Pee <peedylan94@gmail.com>
2025-11-11 20:10:41 +0800

    ✨: Add Transaction model schema

---
+++ backend/src/models/transaction.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const minTwoAccounts = (acc) => {
+  return Array.isArray(acc) && acc.length >= 2;
+};
+
+const transactionSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Transaction must be linked to a user"]
+    },
+    description: {
+      type: String,
+    },
+    entries: {
+      type: [
+        {
+          accountId: {
+            type: mongoose.Types.ObjectId,
+            ref: "Account",
+            required: [true, "Account must be selected"],
+          },
+          type: {
+            type: String,
+            enum: {
+              values: ["debit", "credit"],
+              message: "`{VALUE}` is not a valid type, please choose debit or credit"
+            },
+            required: [true, "Please provide a type: debit or credit"]
+          }
+        }
+      ],
+      validate: {
+        validator: minTwoAccounts,
+        message: "Must be connected to at least 2 accounts"
+      }
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Transaction = mongoose.model("Trasaction", transactionSchema);
+
+export default Transaction;
