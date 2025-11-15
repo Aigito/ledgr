fa66d163588399a487b82efce9177981f4042d2d
Dylan Pee <peedylan94@gmail.com>
2025-11-16 09:08:39 +1030

    🚧: Implement PATCH route for transactions

--- backend/src/routes/transaction.js
+++ backend/src/routes/transaction.js
@@ -5,14 +5,14 @@ import Transaction from "../models/transaction.js";
 const transactionRouter = express.Router();
 
 transactionRouter.get("/", userAuth, async (req, res) => {
-  const { transactionId } = req.body;
+  const { _id } = req.body;
 
   try {
     let transaction;
 
-    if (transactionId) {
+    if (_id) {
       transaction = await Transaction
-        .findById(transactionId)
+        .findById(_id)
         .populate("entries.accountId", "accountName");
     } else {
       transaction = await Transaction.find();
@@ -45,6 +45,24 @@ transactionRouter.post("/", userAuth, async (req, res) => {
   } catch (err) {
     res.status(400).send(err.message)
   }
+});
+
+transactionRouter.patch("/", userAuth, async (req, res) => {
+  const { _id, description, entries } = req.body;
+
+  const updatedTransaction = await Transaction.findOneAndUpdate(
+    { _id },
+    {
+      description,
+      entries
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  );
+
+  res.send(updatedTransaction);
 });
 
 export default transactionRouter;
