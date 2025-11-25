d95ff3c4e797c92a41c6cda9624b0bae278d999b
Dylan Pee <peedylan94@gmail.com>
2025-11-26 08:30:29 +1030

    🚧: Create navbar with vanilla CSS from scratch

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { Link } from 'react-router';
 
 const Navbar = () => {
-  return
+  return (
+    <div className="navbar-container">
+      <h1>My Dashboard</h1>
+      <div className="navbar-link-items">
+        <Link to="/about">About</Link>
+        <Link to="/chart-of-account">Accounts</Link>
+        <Link to="/transaction-journal">Transactions</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar;
